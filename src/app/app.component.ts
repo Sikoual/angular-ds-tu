daca671cef9e3b4a79f2f7dab36cc69c81e00a17
Maxime Hu <maxime@donkeycode.com>
2023-08-13 17:01:09 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,31 @@ export class AppComponent {
   public addTwoNumbers(one: number, two: number){
     return one + two;
   }
+
+  public add(numbers: number[]): number {
+    let sum = 0
+
+    for(const number of numbers){
+      sum += number;
+    }
+
+    return sum;
+  }
+
+  public addMaxFiveNumber(numbers: number[]): number {
+    if(numbers.length > 5){
+      throw new Error('maximum limit number is passed');
+    }
+    let sum = 0
+
+    for(const number of numbers){
+      sum += number;
+    }
+
+    return sum;
+  }
+
+  public transformToNumber(value: string){
+    return +value
+  }
 }
